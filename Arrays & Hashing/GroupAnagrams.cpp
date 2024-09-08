b7ea102c5257ca7d8b9d7fdae8fa462121204388
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2024-09-08 09:46:55 +0530

    Create GroupAnagrams.cpp

---
+++ Arrays & Hashing/GroupAnagrams.cpp
@@ -0,0 +1,24 @@
+// input -> ["eat", "tea", "tan", "ate", "nat", "bat" ]
+// output -> group the anagrams
+// [ [eat, tea, ate], [tan, nat], [bat] ]
+
+// Approach 1 - HashMap and sorting - TC = O(nKLogK) - SC = O(N)
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+  unordered_map<string, vector<string>> map;
+  n = strs.size();
+  string temp;
+
+  for(i=0 --> n){
+    temp = strs[i];
+    // sort the string.
+    sort(strs[i].begin(), strs[i].end());
+    // sorted string is the key, temp string is the value chained to it.
+    map[strs[i]].push_back(temp);
+  }
+  vector<vector<string>> ans;
+  for(auto i=map.begin(); i != map.end(); i++){
+    ans.push_back(i -> second);
+  }
+  return ans;
+}
+
