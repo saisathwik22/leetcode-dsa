2fc101c79cf779b4a0b8faff1acc037c4ea51125
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2024-09-08 16:58:04 +0530

    Solve the problem and write code.

---
+++ Arrays & Hashing/EncodeDecodeString.cpp
@@ -0,0 +1,10 @@
+// LEETCODE Premium Problem:
+
+// Design an algo to encode list of strings to a string.
+// the encoded string then sent over network and decoded back to original list of strings
+// implement encode and decode.
+// input -> ["neet", "code"]
+// output -> ["neet", "code"]
+
+// encode the given string to unreadable sequence by adding some special chars, implement a function to be able to decode it back to its original form.
+
