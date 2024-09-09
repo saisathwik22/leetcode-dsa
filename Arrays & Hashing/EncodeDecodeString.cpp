09e7512833e926016e34b2f8cf8c409b5304f723
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2024-09-09 11:03:04 +0530

    Code added, but not understood.

--- Arrays & Hashing/EncodeDecodeString.cpp
+++ Arrays & Hashing/EncodeDecodeString.cpp
@@ -8,3 +8,28 @@
 
 // encode the given string to unreadable sequence by adding some special chars, implement a function to be able to decode it back to its original form.
 
+string encode(vector<string>& strs) {
+  string result = "";
+
+  for(int i=0; i<strs.size(); i++) {
+    string str = strs[i];
+    result += to_String(str.size()) + "#" + str;
+  }
+  return result;
+}
+
+vector<string> decode(string s){
+  vector<string> result;
+  int i = 0;
+  while(i < s.size()) {
+    int j = i;
+    while(s[j] != '#') {
+      j++;
+    }
+    int length = stoi(s.substr(i, j - i));
+    string str = s.substr(j + 1, length);
+    result.push_back(str);
+    i = j + 1 + length;
+  }
+  return result;
+}
