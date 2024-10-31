b8aa4527f85c87db9b9d03d6194ec03418b05fcc
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2024-10-31 08:15:03 +0530

    Create ReverseNodesKgroup.cpp

---
+++ Linked List/ReverseNodesKgroup.cpp
@@ -0,0 +1,50 @@
+// Leetcode 25 - HARD
+class Solution {
+public:
+    ListNode* reverseLL(ListNode* head) {
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        while(temp != NULL) {
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+    ListNode* getKthNode(ListNode* temp, int k) {
+        k -= 1;
+
+        while(temp != NULL && k > 0) {
+            k--;
+            temp = temp->next;
+        }
+        return temp;
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* temp = head;
+        ListNode* prevLast = NULL;
+
+        while(temp != NULL) {
+            ListNode* kthNode = getKthNode(temp, k);
+            if(kthNode == NULL) {
+                if(prevLast) {
+                    prevLast->next = temp;
+                }
+                break;
+            }
+            ListNode* nextNode = kthNode->next;
+            kthNode->next = NULL;
+
+            reverseLL(temp);
+            if(temp == head) {
+                head = kthNode;
+            } else {
+                prevLast->next = kthNode;
+            }
+            prevLast = temp;
+            temp = nextNode;
+        }
+        return head;
+    }
+};
