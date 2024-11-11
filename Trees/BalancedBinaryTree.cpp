ecd84e493be1db7cf5ac275eeda05d8fd470facd
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2024-11-11 07:15:43 +0530

    Create BalancedBinaryTree.cpp

---
+++ Trees/BalancedBinaryTree.cpp
@@ -0,0 +1,21 @@
+// leetcode 110
+// TC - O(n)
+// SC - O(n) auxillary stack space
+class Solution {
+public:
+    int dfsHeight(TreeNode* root) {
+        if(root == NULL) return 0;
+
+        int lh = dfsHeight(root->left);
+        if(lh == -1) return -1;
+
+        int rh = dfsHeight(root->right);
+        if(rh == -1) return -1;
+
+        if(abs(lh - rh) > 1) return -1;
+        return max(lh, rh) + 1;
+    }
+    bool isBalanced(TreeNode* root) {
+        return dfsHeight(root) != -1;
+    }
+};
